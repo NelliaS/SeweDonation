d81b8ed7ed1181e0eae67d1ae982a91ea83711f7
Lenka Kášová <kasova.lenka@email.cz>
2022-04-29 19:27:29 +0200

    Login form taken out from forms.py.

--- sewedonation/forms.py
+++ sewedonation/forms.py
@@ -15,17 +15,6 @@ class RegistrationForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
-class LogInForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model   = OrganisationProfile
-        fields  = ('email', 'password')
-
-    def __init__(self, *args, **kwargs):
-        super(LogInForm, self).__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-floating mb-2 text-secondary'
 
 
 
